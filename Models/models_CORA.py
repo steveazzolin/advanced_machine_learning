e884bf43cffb0edba0a1c5e9640d4023195e40ea
steveazzolin <steve.azzolin1@gmail.com>
2022-01-24 10:40:15 +0100

    common get_hyperparams

--- Models/models_CORA.py
+++ Models/models_CORA.py
@@ -51,13 +51,8 @@ class GCN_CORA(torch.nn.Module):
     
     def get_hypers(self):
         return {
-            "num_hidden": self.num_hidden,
             "dropout": self.dropout,
             "activation": "ReLU",
-            "weight_decay": self.optimizer.param_groups[0]["weight_decay"],
-            "learning_rate": self.optimizer.param_groups[0]["lr"],
-            "optimizer": self.optimizer.__class__.__name__,
-            "num_epochs": self.num_epochs
         }
 
 
@@ -86,14 +81,9 @@ class GAT_CORA(torch.nn.Module):
 
     def get_hypers(self):
         return {
-            "num_hidden": self.num_hidden,
             "dropout": self.dropout,
             "num_heads": self.num_heads,
             "activation": "ELU",
-            "weight_decay": self.optimizer.param_groups[0]["weight_decay"],
-            "learning_rate": self.optimizer.param_groups[0]["lr"],
-            "optimizer": self.optimizer.__class__.__name__,
-            "num_epochs": self.num_epochs
         }
 
         
