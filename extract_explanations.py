0838eb8495478bb5c178deae815f51b02176dd24
steveazzolin <steve.azzolin1@gmail.com>
2022-02-11 21:38:53 +0100

    parametrized num_epochs

--- extract_explanations.py
+++ extract_explanations.py
@@ -7,6 +7,7 @@ import Models.models_CORA as models_CORA
 import Models.models_MUTAG as models_MUTAG
 import Models.models_REDDIT as models_REDDIT
 import Models.models_BAshapes as models_BAshapes
+import Models.models_CITESEER as models_CITESEER
 
 from Explainers.explainers import * #SemiSupSubGraphX, SemiSupPGExplainer, LargeSemiSupPGExplainer
 
@@ -16,11 +17,11 @@ def explain_SubGraphX(model, dataset, dataset_name, model_name):
     fw.explain(max_nodes=10)
 
 
-def explain_PGExplainer(framework, dataset_name, model_name, save):
+def explain_PGExplainer(framework, dataset_name, model_name, save, num_epochs):
     if dataset_name == "REDDIT":
-        fw = LargeSemiSupPGExplainer(framework, dataset_name, model_name, num_epochs=3, num_hops=3)
+        fw = LargeSemiSupPGExplainer(framework, dataset_name, model_name, num_epochs=num_epochs, num_hops=3)
     else:
-        fw = SemiSupPGExplainer(framework, dataset_name, model_name, num_epochs=2, num_hops=3)
+        fw = SemiSupPGExplainer(framework, dataset_name, model_name, num_epochs=num_epochs, num_hops=3)
     fw.explain(top_k=5, save=save)
 
 
@@ -42,18 +43,23 @@ if __name__ == "__main__":
     assert args.model + "_" + args.dataset in pretrained_models , "Model not yet implemented or trained"
     assert args.expl.lower() in explainers , "Explainer not yet implemented"
 
-
+    num_epochs = 20
     if args.dataset.upper() == "CORA":
+        num_epochs = 20
         fw = models_CORA.getFrameworkByName(args.model.upper())
     elif args.dataset.upper() == "MUTAG":
         fw = models_MUTAG.getFrameworkByName(args.model.upper())
     elif args.dataset.upper() == "REDDIT":
+        num_epochs = 3
         fw = models_REDDIT.getFrameworkByName(args.model.upper(), batch_size=512, num_workers=4)
     elif args.dataset.upper() == "BASHAPES":
+        num_epochs = 40
         fw = models_BAshapes.getFrameworkByName(args.model.upper())
+    elif args.dataset.upper() == "CITESEER":
+        fw = models_CITESEER.getFrameworkByName(args.model.upper())
     fw.load_model()
 
     if args.expl.lower() == "subgraphx":
         explain_SubGraphX(fw.model, fw.dataset, args.dataset.upper(), args.model.upper())
     elif args.expl.lower() == "pgexplainer":
-        explain_PGExplainer(fw, args.dataset.upper(), args.model.upper(), args.save)
+        explain_PGExplainer(fw, args.dataset.upper(), args.model.upper(), args.save, num_epochs)
