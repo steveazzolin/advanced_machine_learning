5015598571752b479cd792a3cf49913e29e13487
steveazzolin <steve.azzolin1@gmail.com>
2022-03-09 11:16:38 +0100

    fix

--- mine_explanations.py
+++ mine_explanations.py
@@ -628,7 +628,7 @@ if __name__ == "__main__":
     #plot_edge_weight_distribution_hist(expls)
 
     expls = preprocess_explanations(expls, cut_edges=args.cut_edges, cut_cc=args.cut_cc)
-    #expls_unique = find_unique_explanations(expls, log=True)
+    expls_unique = find_unique_explanations(expls, log=True)
 
     #plot_k_per_class(expls_unique, labels=fw.dataset.data.y, k=args.k)
     
@@ -647,5 +647,5 @@ if __name__ == "__main__":
     ##
     # METHOD 2: Learn features via graph classification
     ##
-    embs = learn_features_per_graph(expls, args.model, args.dataset, log=True)
+    embs = learn_features_per_graph(expls_unique, args.model, args.dataset, log=True)
     visualize_embeddings(embs, k=args.k)
